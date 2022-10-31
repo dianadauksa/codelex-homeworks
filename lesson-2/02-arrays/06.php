d70cc29e85201e2759a3e49d9c63e0fb16fc7a92
Diana D <91746961+dianadauksa@users.noreply.github.com>
2022-10-31 21:44:23 +0200

    Add 'again' or 'quit' feature

--- lesson-2/02-arrays/06.php
+++ lesson-2/02-arrays/06.php
@@ -1,38 +1,46 @@
 <?php
 
-$words = ['codelex', 'programming', 'typescript', 'javascript', 'bootcamp', 'acceleration', 'gravity'];
-$word = $words[array_rand($words)];
-$letters = str_split($word);
-$targetWord = str_split(str_repeat('_', strlen($word)));
-$misses = [];
-$guesses = 0;
-$maxGuesses= strlen($word) + 3;
+$gameStatus = 'again';
+function game() {
+    $words = ['codelex', 'programming', 'typescript', 'javascript', 'bootcamp', 'acceleration', 'gravity'];
+    $word = $words[array_rand($words)];
+    $letters = str_split($word);
+    $targetWord = str_split(str_repeat('_', strlen($word)));
+    $misses = [];
+    $guesses = 0;
+    $maxGuesses= strlen($word) + 3;
 
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-" . PHP_EOL;
-echo "Word: " . implode(' ', $targetWord) . PHP_EOL;
-echo "Misses: " . implode($misses) . PHP_EOL;
-
-while($guesses<$maxGuesses) {
-    $response = readline('Guess: ');
     echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-" . PHP_EOL;
-    $letterPositions = array_keys($letters, $response);
-
-    if(count($letterPositions)>0) {
-        foreach($letterPositions as $position){
-            $targetWord[$position] = $response;
-        }
-        if($word == implode('', $targetWord)) {
-            echo 'Correct! The word is: ' . $word . PHP_EOL;
-            break;
-        }
-    } else {
-        $misses[] = $response;
-    }
-    $guesses++;
     echo "Word: " . implode(' ', $targetWord) . PHP_EOL;
     echo "Misses: " . implode($misses) . PHP_EOL;
-}
 
-if ($word != implode('', $targetWord)) {
-    echo 'Wrong! The word was: ' . $word . PHP_EOL;
+    while($guesses<$maxGuesses) {
+        $response = readline('Guess: ');
+        echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-" . PHP_EOL;
+        $letterPositions = array_keys($letters, $response);
+
+        if(count($letterPositions)>0) {
+            foreach($letterPositions as $position){
+                $targetWord[$position] = $response;
+            }
+            if($word == implode('', $targetWord)) {
+                echo 'Correct! The word is: ' . $word . PHP_EOL;
+                echo "Play 'again' or 'quit'? ";
+                return readline();
+            }
+        } else {
+            $misses[] = $response;
+        }
+        $guesses++;
+        echo "Word: " . implode(' ', $targetWord) . PHP_EOL;
+        echo "Misses: " . implode($misses) . PHP_EOL;
+    }
+
+    if ($word != implode('', $targetWord)) {
+        echo 'Wrong! The word was: ' . $word . PHP_EOL;
+        echo "Play 'again' or 'quit'? ";
+        return readline();
+    }
 }
+while ($gameStatus === "again") $gameStatus = game();
+echo "Bye, thanks for playing!".PHP_EOL;
