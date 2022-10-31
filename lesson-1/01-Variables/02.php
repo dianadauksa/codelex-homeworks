fe34d5e32acfab9215b823a13234adb1370af175
Diana D <91746961+dianadauksa@users.noreply.github.com>
2022-10-31 16:41:08 +0200

    Add files via upload

---
+++ lesson-1/01-Variables/02.php
@@ -0,0 +1,7 @@
+<?php
+
+$regular_num = 10;
+$floating_num = 10.10;
+$greeting = 'hello world';
+
+var_dump($regular_num, $floating_num, $greeting);
---
+++ lesson-1/01-Variables/03.php
@@ -0,0 +1,6 @@
+<?php
+$my_name = 'Diana';
+$my_surname = 'Dauksa';
+$my_age = 26;
+
+echo $my_name . ' ' . $my_surname . ', age: ' . $my_age . PHP_EOL;
