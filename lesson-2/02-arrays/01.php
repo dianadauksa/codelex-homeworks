e56bf1aafa768272b60d082084f2d2c96fea086d
Diana D <91746961+dianadauksa@users.noreply.github.com>
2022-10-31 16:52:30 +0200

    Create 01.php

---
+++ lesson-2/02-arrays/01.php
@@ -0,0 +1,30 @@
+<?php
+
+$numbers = [
+    1789, 2035, 1899, 1456, 2013,
+    1458, 2458, 1254, 1472, 2365,
+    1456, 2165, 1457, 2456
+];
+$numbers2 = [...$numbers];
+$numbersSorted = sort($numbers2);
+
+//todo
+echo "Original numeric array: " . implode(", ", $numbers) . PHP_EOL;
+//todo
+echo "Sorted numeric array: " . implode(", ", $numbers2) . PHP_EOL;
+
+$words = [
+    "Java",
+    "Python",
+    "PHP",
+    "C#",
+    "C Programming",
+    "C++"
+];
+$words2 = [...$words];
+$wordsSorted= sort($words2);
+
+//todo
+echo "Original string array: " . implode(", ", $words) . PHP_EOL;
+//todo
+echo "Sorted string array: " . implode(", ", $words2) . PHP_EOL;
