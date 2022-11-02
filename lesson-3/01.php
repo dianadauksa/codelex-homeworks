17db5ef57b029f3c37b9d096a4ab0a41dae3bf1e
Diana D <91746961+dianadauksa@users.noreply.github.com>
2022-11-02 19:35:37 +0200

    Create 01.php

---
+++ lesson-3/01.php
@@ -0,0 +1,13 @@
+<?php
+$numbers = [];
+echo "Input the 1st number: ";
+$numbers []= readline();
+
+echo "Input the 2nd number: ";
+$numbers []= readline();
+
+echo "Input the 3rd number: ";
+$numbers []= readline();
+
+//todo print the largest number
+echo "The largest number is: " . max($numbers) . PHP_EOL;
