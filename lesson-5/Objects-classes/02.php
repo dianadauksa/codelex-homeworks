386a1576f0e54d7d7193a0b79160b431f3264178
Diana <dianadauksa@gmail.com>
2022-11-05 21:19:31 +0200

    Update classes 02.php

--- lesson-5/Objects-classes/02.php
+++ lesson-5/Objects-classes/02.php
@@ -1,1 +1,27 @@
 <?php
+
+class Point {
+    public int $x;
+    public int $y;
+    function __construct(int $x, int $y)
+    {
+        $this-> x = $x;
+        $this-> y = $y;
+    }
+
+    public function swapPoints (object $a, object $b) {
+        $a->x ^= $b->x; // bitwise operator, bitwise XOP operator - returns bit-by-bit XOR of input values;
+        $b->x ^= $a->x;
+        $a->x ^= $b->x;
+
+        $a->y ^= $b->y;
+        $b->y ^= $a->y;
+        $a->y ^= $b->y;
+    }
+}
+
+$p1 = new Point(5, 2);
+$p2 = new Point(-3, 6);
+$p1->swapPoints($p1, $p2);
+echo "(" . $p1->x . ", " . $p1->y . ")";
+echo "(" . $p2->x . ", " . $p2->y . ")";
