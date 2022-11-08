1cd9cecdecbbb899d5578a767bfe2128c7cf1444
Diana <dianadauksa@gmail.com>
2022-11-08 12:41:33 +0200

    Update 03.php

--- lesson-5/Objects-classes/03.php
+++ lesson-5/Objects-classes/03.php
@@ -13,16 +13,19 @@ class FuelGauge {
     }
 
     public function incrementFuelTank(int $liters = 1): void {
-        if ($this->currentAmountOfFuel < $this -> maxAmountOfFuel) {
+        if ($this->currentAmountOfFuel + $liters > $this->maxAmountOfFuel) {
+            $this->currentAmountOfFuel = $this->maxAmountOfFuel;
+        } else {
             $this->currentAmountOfFuel += $liters;
         }
     }
 
     public function decrementFuelTank() {
-        if($this -> currentAmountOfFuel > 0) {
-            $this->currentAmountOfFuel--;
+        $minAmountOfFuel = 0;
+        if ($this->currentAmountOfFuel <= $minAmountOfFuel) {
+            $this->currentAmountOfFuel = $minAmountOfFuel;
         } else {
-            return "No more fuel in tank! :(\n";
+            $this->currentAmountOfFuel--;
         }
     }
 }
@@ -32,22 +35,22 @@ class Odometer {
     private int $initialMileage;
     private int $maxMileage = 999999;
     private int $kilometersPerLiter = 10;
-    function __construct(int $currentMileage = 0, int $initialMileage = 0)
+    function __construct(int $currentMileage = 0)
     {
         $this-> currentMileage = $currentMileage;
-        $this-> initialMileage = $initialMileage;
+        $this->initialMileage = $currentMileage;
     }
 
     public function getCurrentMileage(): string {
         return $this -> currentMileage . ' kilometers' . PHP_EOL;
     }
 
-    public function incrementMileage(): void {
-        if ($this->currentMileage < $this->maxMileage) {
-            $this->currentMileage++;
-        }
-        if ($this -> currentMileage === $this->maxMileage) {
+    public function incrementMileage(int $amount = 1): void {
+        if ($amount < 0) return;
+        if ($this -> currentMileage + $amount >= $this->maxMileage) {
             $this->currentMileage = 0;
+        } else {
+            $this->currentMileage += $amount;
         }
     }
 
@@ -61,8 +64,8 @@ class Odometer {
 $fuelGauge = new FuelGauge(50);
 $odometer = new Odometer(10, 10);
 
-$fuelGauge->incrementFuelTank(10);
-echo $fuelGauge->getCurrentAmountOfFuel(); // 60 liters
+$fuelGauge->incrementFuelTank(30);
+echo $fuelGauge->getCurrentAmountOfFuel(); // 70 liters, even though filled 50 + 30 liters
 
 while ($fuelGauge->getCurrentAmountOfFuel() > 0) {
     $odometer->incrementMileage();
